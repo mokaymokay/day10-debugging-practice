1ff9916ed1bd335cd87b814ac82f64a7578f1b03
Kay Mok <kaymok3@gmail.com>
2018-03-10 15:01:56 -0500

    Refactor

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,29 +6,30 @@ function randomFrom(array) {
 }
 
 function checkInput(input, computerChoices) {
+  let computerChoice = randomFrom(computerChoices);
+  let choices = `You chose ${input} and the computer chose ${computerChoice}.`; // added choices statement for reuse
+
   if (input === "quit") {
     return true;
   };
-
-  let computerChoice = randomFrom(computerChoices);
 
   if (input !== "scissors" && input !== "paper" && input !== "rock") {
     alert("Please enter a valid input: rock/paper/scissors, or quit to stop playing. ");
     return false;
   } else if (computerChoice === "rock" && input === "scissors") {
-    alert(`You chose ${input} and the computer chose ${computerChoice}.\nComputer wins!`);
+    alert(choices + " Computer wins!");
     return false;
   } else if (computerChoice === "scissors" && input === "paper") {
-    alert(`You chose ${input} and the computer chose ${computerChoice}.\nComputer wins!`);
+    alert(choices + " Computer wins!");
     return false;
   } else if (computerChoice === "paper" && input === "rock") {
-    alert(`You chose ${input} and the computer chose ${computerChoice}.\nComputer wins!`);
+    alert(choices + " Computer wins!");
     return false;
   } else if (computerChoice === input) {
-    alert(`You chose ${input} and the computer chose ${computerChoice}.\nIt's a tie!`); // added scenario when user ties with computer
+    alert(choices + " It's a tie!"); // added scenario when user ties with computer
     return false;
   } else {
-    alert(`You chose ${input} and the computer chose ${computerChoice}.\nYou win!`); // added scenario when user wins
+    alert(choices + " You win!"); // added scenario when user wins
     return false;
   };
 }
