c9a6e96ba3e1a340ba8b988fb1a10b9f53631c29
Kay Mok <kaymok3@gmail.com>
2018-03-12 10:44:01 -0400

    Refactor and add comment

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -15,27 +15,23 @@ function checkInput(input, computerChoices) {
 
   if (input !== "scissors" && input !== "paper" && input !== "rock") {
     alert("Please enter a valid input: rock/paper/scissors, or quit to stop playing. ");
-    return false;
   } else if (computerChoice === "rock" && input === "scissors") {
     alert(choices + " Computer wins!");
-    return false;
   } else if (computerChoice === "scissors" && input === "paper") {
     alert(choices + " Computer wins!");
-    return false;
   } else if (computerChoice === "paper" && input === "rock") {
     alert(choices + " Computer wins!");
-    return false;
   } else if (computerChoice === input) {
     alert(choices + " It's a tie!"); // added scenario when user ties with computer
-    return false;
   } else {
     alert(choices + " You win!"); // added scenario when user wins
-    return false;
   };
+  return false;
 }
 
 function start(gameOver, computerChoices) {
   while (!gameOver) {
+    // assigned player input to prompt instead of empty string
     let playerInput = prompt("Hi! Enter rock/paper/scissors to play, or quit to stop playing.");
     gameOver = checkInput(playerInput, computerChoices);
     // playerInput = "paper";
